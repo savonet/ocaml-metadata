39b6218370920824ea7d218c58e6c20a9ca4b26b
Samuel Mimram <smimram@gmail.com>
2022-11-22 16:34:16 +0100

    Handle basic charset conversion.

--- examples/meta.ml
+++ examples/meta.ml
@@ -1,8 +1,10 @@
 let () =
+  let binary = ref false in
   let fname = ref [] in
   let format = ref "" in
   Arg.parse
     [
+      ("-b", Arg.Set binary, "Show binary contents of tags.");
       ("--format", Arg.Set_string format, "File format.");
       ("-f", Arg.Set_string format, "File format.");
     ]
@@ -44,7 +46,9 @@ let () =
       let m = parser fname in
       List.iter
         (fun (k, v) ->
-          let v = if k = "APIC" || k = "PIC" then "<redacted>" else v in
-          Printf.printf "- %s: %S\n%!" k v)
+          let v = if k = "APIC" || k = "PIC" || k = "RVA2" then "<redacted>" else v in
+          Printf.printf "- %s: %s\n%!" k v;
+          if !binary then Printf.printf "  %s: %S\n%!" k v
+        )
         m)
     fname
--- src/metadataCharEncoding.ml
+++ src/metadataCharEncoding.ml
@@ -3,5 +3,28 @@ module type T = sig
 end
 
 module Naive : T = struct
-  let convert ?source:_ s = s
+  let convert ?source s =
+    let source = match source with None -> `UTF_8 | Some x -> x in
+    let buf = Buffer.create 10 in
+    let result () = Buffer.contents buf in
+    match source with
+    | (`UTF_16LE | `UTF_16BE) as source ->
+       let get_char =
+         match source with
+         | `UTF_8 -> String.get_utf_8_uchar
+         | `UTF_16LE -> String.get_utf_16le_uchar
+         | `UTF_16BE -> String.get_utf_16be_uchar
+       in
+       let len = String.length s in
+       let rec f pos =
+         if pos = len then result ()
+         else
+           let d = get_char s pos in
+           let c = Uchar.utf_decode_uchar d in
+           Buffer.add_utf_8_uchar buf c;
+           f (pos + Uchar.utf_decode_length d)
+       in
+       f 0
+    | `UTF_8 -> s
+    | _ -> s
 end
