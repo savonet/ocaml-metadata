152b11ba5c6332777a31b307da5317d9ba68f246
Samuel Mimram <smimram@gmail.com>
2022-11-22 16:44:55 +0100

    Handle endianness.

--- src/metadataCharEncoding.ml
+++ src/metadataCharEncoding.ml
@@ -5,18 +5,33 @@ end
 module Naive : T = struct
   let convert ?source s =
     let source = match source with None -> `UTF_8 | Some x -> x in
+    let endianness = ref `BE in
     let buf = Buffer.create 10 in
     match source with
-    | (`UTF_16LE | `UTF_16BE) as source ->
+    | (`UTF_16 | `UTF_16LE | `UTF_16BE) as source ->
        let get_char =
          match source with
          | `UTF_16LE -> String.get_utf_16le_uchar
          | `UTF_16BE -> String.get_utf_16be_uchar
+         | `UTF_16 ->
+            match !endianness with
+            | `LE -> String.get_utf_16le_uchar
+            | `BE -> String.get_utf_16be_uchar
        in
        let len = String.length s in
        let rec f pos =
          if pos = len then
            Buffer.contents buf
+         else if pos + 2 <= len && s.[pos] = '\xfe' && s.[pos] = '\xff' then
+           (
+             endianness := `BE;
+             f (pos + 2)
+           )
+         else if pos + 2 <= len && s.[pos] = '\xff' && s.[pos] = '\xfe' then
+           (
+             endianness := `LE;
+             f (pos + 2)
+           )
          else
            let d = get_char s pos in
            let c = Uchar.utf_decode_uchar d in
