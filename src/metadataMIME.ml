dac168468e3b1fb55ff09c58a2f46c2120fad3a6
Samuel Mimram <smimram@gmail.com>
2024-01-29 22:37:37 +0100

    wave file.

--- src/metadataMIME.ml
+++ src/metadataMIME.ml
@@ -1,5 +1,14 @@
 (** Guess the mime-type of a file. *)
 
+module String = struct
+  include String
+
+  let contains_at offset ~substring s =
+    let n = String.length substring in
+    if String.length s < offset + n then false
+    else String.sub s offset n = substring
+end
+
 let prefixes =
   [
     "ID3", "audio/mpeg";
@@ -8,9 +17,26 @@ let prefixes =
     "\137PNG\013\010\026\010", "image/png";
   ]
 
+let advanced =
+  let wav s =
+    String.starts_with ~prefix:"RIFF" s &&
+    String.contains_at 8 ~substring:"WAVEfmt " s
+  in
+  [
+    wav, "audio/wav"
+  ]
+
 let of_string s =
   let ans = ref "" in
   try
+    List.iter
+      (fun (f, mime) ->
+         if f s then
+           (
+             ans := mime;
+             raise Exit
+           )
+      ) advanced;
     List.iter
       (fun (prefix, mime) ->
          if String.starts_with ~prefix s then
@@ -24,7 +50,7 @@ let of_string s =
   | Exit -> !ans
 
 let of_file fname =
-  let len = 10 in
+  let len = 16 in
   let buf = Bytes.create len in
   let ic = open_in fname in
   let n = input ic buf 0 len in
